30981778500ac62814c0fadba1373d64e153707c
Erik-GT-97 <erik.fisi16@gmail.com>
2021-02-13 19:05:36 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+dotenv.config()
+
+const app = express()
+
+app.use(bodyParser.json({
+    limit: "30mb"
+}))
+app.use(bodyParser.urlencoded({
+    limit: "30mb",
+    extended: true
+}))
+
+app.use(cors())
+
+app.get('/', ( req, res) => {
+    res.send('Hello API ONEPIECE')
+})
+
+const port = process.env.PORT || 5000 
+
+mongoose.connect(process.env.CONNECTION_URL , { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+  .then(() => app.listen(port , () => console.log(`Sever running on port : ${port}`)))
+  .catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false)
+
